fb688463505ac5e63ff943df356d14b4fd8995ea
jhonbriveram <jhonbriveram@gmail.com>
2022-06-09 20:31:54 -0500

    Repo

---
+++ CapaNegocio/Prestamo.cs
@@ -0,0 +1,27 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+   public class Prestamo
+    {
+        //public static  Prestamo Registrar()
+        //{
+        //    using (var db = new FinancieraEntities())
+        //    {
+        //        return db.Prestamo;
+        //    }
+        //}
+        public static List<CapaDatos.Prestamo> Listar()
+        {
+            using(var db = new FinancieraEntities())
+            {
+                return db.Prestamo.ToList();
+            }
+        }
+    }
+}
---
+++ CapaPresentacion/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CapaNegocio;
+using CapaPresentacion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CapaPresentacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var listado = Prestamo.Listar();
+            return View(listado);
+        }
+
+        public IActionResult Registrar()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
